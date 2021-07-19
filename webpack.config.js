07c0186eef9f86056cf131758e6d250c2e989d5b
Kyle Moxham <kyle.moxham@built.co.uk>
2021-07-19 07:54:04 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins:[
+      new HtmlWebPackPlugin({
+         template: path.resolve( __dirname, 'public/index.html' ),
+         filename: 'index.html'
+      })
+  ],
+  module:{
+      rules:[
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+            test:/\.js$/,
+            exclude:/node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env','@babel/preset-react']
+              }
+            }
+        }
+      ],
+
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      src: path.resolve(__dirname, 'src/')
+  }
+};
